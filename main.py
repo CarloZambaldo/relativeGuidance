2e6178a57fb8952bbadd193424d36214fdde9ec3
Carlo Zambaldo <carlo.zambaldo@gmail.com>
2024-12-19 13:08:44 +0100

    ok

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-"""import sys
+import sys
 import os
 
+"""
 # Absolute path to your project directory
-project_path = os.path.abspath("C:\Users\ASUS\OneDrive - Politecnico di Milano\UNIVERSITY\MATERIE\MAGISTRALE\TESI\\1-CODE\simulationFull")  # Change this to your project path
+project_path = os.path.abspath(r"C:\Users\ASUS\OneDrive - Politecnico di Milano\UNIVERSITY\MATERIE\MAGISTRALE\TESI\1-CODE\simulationFull")  # Change this to your project path
 
 # Add the path to sys.path if it's not already present
 if project_path not in sys.path:
-    sys.path.append(project_path)
+	sys.path.append(project_path)
 
 #exec(env_config)
-"""
+
 from config import *
 from generalScripts import * 
 from RLEnvironment import *
+"""
